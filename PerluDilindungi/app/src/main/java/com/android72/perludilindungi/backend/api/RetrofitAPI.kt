d90ded17abef78e8651ce5c19382dc9728ea9bbe
afifahfq <afifahfq@gmail.com>
2022-03-02 12:11:05 +0700

    create recycle view

--- PerluDilindungi/app/src/main/java/com/android72/perludilindungi/backend/api/RetrofitAPI.kt
+++ PerluDilindungi/app/src/main/java/com/android72/perludilindungi/backend/api/RetrofitAPI.kt
@@ -1,11 +1,16 @@
 package com.android72.perludilindungi.backend.api
 
 import com.android72.perludilindungi.backend.model.CheckinData
+import com.android72.perludilindungi.backend.model.FaskesData
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface RetrofitAPI {
     @POST("check-in")
     fun checkIn(@Body checkinData: CheckinData): Call<CheckinData>
+
+    @GET("get-faskes-vaksinasi")
+    fun getFaskes(@Body faskesData: FaskesData): Call<FaskesData>
 }
