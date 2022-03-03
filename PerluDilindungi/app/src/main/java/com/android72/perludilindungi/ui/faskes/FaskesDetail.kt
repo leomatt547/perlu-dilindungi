8fedabfeb869860660c42662731f31b884961f57
findraya <13519171@std.stei.itb.ac.id>
2022-03-03 22:22:13 +0700

    add faskes search and detail layout

--- PerluDilindungi/app/src/main/java/com/android72/perludilindungi/ui/faskes/FaskesDetail.kt
+++ PerluDilindungi/app/src/main/java/com/android72/perludilindungi/ui/faskes/FaskesDetail.kt
@@ -9,14 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android72.perludilindungi.databinding.FragmentFaskesBinding
+import com.android72.perludilindungi.databinding.FragmentDetailBinding
 
-class FaskesFragment : Fragment() {
+class FaskesDetail : Fragment() {
 
     //private lateinit var faskesViewModel: FaskesViewModel
-    private var _binding: FragmentFaskesBinding? = null
-    //private lateinit var recyclerView: RecyclerView;
+    private var _binding: FragmentDetailBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,13 +29,9 @@ class FaskesFragment : Fragment() {
             ViewModelProvider(this).get(FaskesViewModel::class.java)
          */
 
-        _binding = FragmentFaskesBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
 
         val root: View = binding.root
-        /*recyclerView = _binding!!.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(_binding!!.fragmentFaskes.context);
-        recyclerView.adapter = FaskesAdapter();
-         */
 
         /*val textView: TextView = binding.textHome
         faskesViewModel.text.observe(viewLifecycleOwner, Observer {
