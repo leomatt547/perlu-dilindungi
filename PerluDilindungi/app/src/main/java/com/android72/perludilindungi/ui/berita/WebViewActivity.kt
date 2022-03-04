7f355e8df211da849d4735994ddc03382b8e339e
leo.matt.547 <leo.matt.547@gmail.com>
2022-03-04 14:44:50 +0700

    adding error webview

--- PerluDilindungi/app/src/main/java/com/android72/perludilindungi/ui/berita/WebViewActivity.kt
+++ PerluDilindungi/app/src/main/java/com/android72/perludilindungi/ui/berita/WebViewActivity.kt
@@ -2,16 +2,17 @@ package com.android72.perludilindungi.ui.berita
 
 import android.net.http.SslError
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.View
-import android.webkit.SslErrorHandler
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.android72.perludilindungi.R
 import com.android72.perludilindungi.databinding.WebBeritaBinding
+import kotlinx.android.synthetic.main.web_berita.*
 
 class WebViewActivity : AppCompatActivity() {
 //    private val arguments : WebViewFragmentArgs by navArgs();
@@ -27,7 +28,7 @@ class WebViewActivity : AppCompatActivity() {
 
         val newsUrl = intent.getStringExtra("URL_TO_WEB")!!
         Log.v("tag", newsUrl)
-        webView = binding.newsWeb
+        webView = newsWeb
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
@@ -44,6 +45,14 @@ class WebViewActivity : AppCompatActivity() {
                 if (handler != null) {
                     handler.proceed()
                 };
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                Toast.makeText(this@WebViewActivity, "Got Error! $error", Toast.LENGTH_SHORT).show()
             }
         }
         webView.loadUrl(newsUrl);
